7b50a2f2070839a883db0b6e640a186a42eda1ca
wmeissner <wmeissner@05a1902d-0e30-0410-9027-c8643ccd90df>
2007-05-14 02:34:51 +0000

    Add copyright
    git-svn-id: https://jna.dev.java.net/svn/jna/branches/wmeissner-jnalib@49 05a1902d-0e30-0410-9027-c8643ccd90df

--- test/com/sun/jna/VarArgsTest.java
+++ test/com/sun/jna/VarArgsTest.java
@@ -1,18 +1,21 @@
-/*
- * VarArgsTest.java
- * JUnit based test
+/* Copyright (c) 2007 Wayne Meissner, All Rights Reserved
  *
- * Created on 14 May 2007, 11:20
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.  
  */
+
 
 package com.sun.jna;
 
 import junit.framework.TestCase;
 
-/**
- *
- * @author wayne
- */
 public class VarArgsTest extends TestCase {
     public static interface TestLibrary extends Library {
         TestLibrary INSTANCE = (TestLibrary)
